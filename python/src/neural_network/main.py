0262f29e115ed452a6d3b3773ab28a2edff3551c
Ferdinando222 <71137846+Ferdinando222@users.noreply.github.com>
2023-04-30 10:25:26 +0200

    neural network v1.0

--- python/src/neural_network/main.py
+++ python/src/neural_network/main.py
@@ -1,98 +1,72 @@
-#%%
+# %%
+# IMPORT LIBRARY
 import tensorflow as tf
+import functions_train as ft
 import numpy as np
-print("ONE")
-from keras.utils import plot_model
-from keras.layers import Input, Conv1D, MaxPooling1D, BatchNormalization, Concatenate
-from keras.layers import Bidirectional, LSTM, Dropout, Dense, ZeroPadding1D
-from keras.callbacks import ReduceLROnPlateau
-print('keras')
-import glob
-import sklearn
-from sklearn.model_selection import KFold, train_test_split
-from sklearn.metrics import mean_squared_error, r2_score
-print('sklearn')
-
-
-import os
-print('OS')
+from os import path
+from tensorflow.keras.callbacks import EarlyStopping, train_test_split
+from sklearn.model_selection import KFold
 import training
 
-print("DONE IMPORT")
 
-################################################### NEURAL NETWORK ###################################################
-#%% Build th Neural Network
-
-# Define input shape
-input_shape = (22050,1)
-
-# Define input layer
-input_layer = Input(shape=input_shape)
-
-# Define fine-view CNN
-fine_view = Conv1D(filters=8, kernel_size=32, strides=8, activation='relu')(input_layer)
-fine_view = BatchNormalization()(fine_view)
-fine_view = MaxPooling1D(pool_size=8)(fine_view)
-
-# Define coarse-view CNN
-coarse_view = Conv1D(filters=8, kernel_size=128, strides=32, activation='relu')(input_layer)
-coarse_view = BatchNormalization()(coarse_view)
-coarse_view = MaxPooling1D(pool_size=2)(coarse_view)
-
-# Pad the coarse-view CNN to match the number of time steps in the fine-view CNN
-coarse_view = ZeroPadding1D(padding=((0, 1)))(coarse_view)
-
-# Merge the two CNNs
-merged = Concatenate(axis=-1)([fine_view, coarse_view])
-
-# Define Bidirectional LSTM layers
-lstm = Dropout(rate=0.5)(merged)
-lstm = Bidirectional(LSTM(units=32, return_sequences=True))(lstm)
-lstm = Bidirectional(LSTM(units=32))(lstm)
-lstm = Dropout(rate=0.5)(lstm)
-
-# Define output layer
-output_layer = Dense(units=2, activation='tanh')(lstm)
-
-# Define the final model
-model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
-
-#%% Plot Neural Network
-# plot_model(model, to_file='FirstNN.png', show_shapes=True, show_layer_names=True)
+# Load meta.csv containing file-paths and labels as pd.DataFrame
+# %%
+path, labels = ft.extract_input_target()
+X_train, X_test, y_train, y_test = train_test_split(
+    path, labels, test_size=0.1, random_state=42)
+ds_train = ft.get_dataset(X_train, y_train)
+ds_test = ft.get_dataset(X_test, y_test)
 
 
 ####################################################### FIT #######################################################
-#%% Model Fit
+# Definizione del numero di fold della cross-validation
+# %%
+num_folds = 10
+batch_size = 1024
+len_dataset = len(list(ds_train.as_numpy_iterator()))*0.8
+fold_size = len_dataset//num_folds
+kf = KFold(n_splits=num_folds, shuffle=True)
+best_rmse = 1
+for fold, (train_indexes, val_indexes) in enumerate(kf.split(X_train)):
+    print('Starting Fold:', fold+1)
+    start, end = fold * fold_size, (fold + 1) * fold_size
 
-model.compile(optimizer='adam',
-              loss='mse')
-# print(model.summary())
-train_files = glob.glob(os.path.join(training.OUTPUT_DIR_TRAIN, '*.pkl'))
+    # Definizione degli insiemi di training e di validation per questo fold
+    val_data = ds_train.take(len(val_indexes))
+    train_data = ds_train.skip(len(val_indexes))
+    print("split dataset----")
+    train_steps = int(train_data.cardinality().numpy()//batch_size)
+    print(train_steps)
+    val_steps = int(val_data.cardinality().numpy()//batch_size)
+    print(val_steps)
 
-X, y_v, y_a = training.get_data(train_files)
-y = np.column_stack((y_v, y_a))
-kf = KFold(n_splits=10)
-rms = []
-rms_test = []
-X, X_remain, y_train, y_remain = train_test_split(X, y, train_size=0.9)
-for train_index, validation_index in kf.split(X):
-    print("TRAIN:", train_index, "TEST:", validation_index)
-    X_train, X_validation = X[train_index], X[validation_index]
-    y_train, y_validation = y[train_index], y[validation_index]
-    # tf.convert_to_tensor(X_train, dtype=tf.float32)
-    # tf.convert_to_tensor(y_train, dtype=tf.float32)
-    # Split the data into training, validation, and test sets with 8:1:1 ratio
-    model.fit(x=X_train,
-              y=y_train,
-              batch_size=128,
-              epochs=10,
-              verbose=1,
-              shuffle=True)
-    model.save("model.h5")
-    y_val_pred = model.predict(X_validation)
-    rms.append(np.sqrt(mean_squared_error(y_validation, y_val_pred)))
-    y_test_predict = model.predict(X_remain)
-    rms_test.append(np.sqrt(mean_squared_error(y_remain, y_test_predict)))
-    
-    
-#%%
+    train_data = ft.prepare_for_training(train_data)
+    val_data = ft.prepare_for_training(val_data)
+    print("prepare for training")
+    # Creazione e addestramento del modello per questo fold
+    model = ft.create_model()
+    early_stop = EarlyStopping(
+        monitor='val_loss',  # monitora la loss sul set di validazione
+        patience=10,  # numero di epoche consecutive senza miglioramento
+        mode='min',  # minimizza la loss
+        verbose=1  # stampa messaggi durante l'addestramento
+    )
+    model.fit(train_data, epochs=1000, validation_data=val_data, verbose=1,
+              validation_steps=val_steps, steps_per_epoch=train_steps, callbacks=[early_stop])
+    print('Evaluate------')
+    loss, mse, rmse = model.evaluate(train_data, steps=train_steps)
+    print('LOSS:', loss)
+    print('mse:', mse)
+    print('rmse:', rmse)
+    if rmse < best_rmse:
+        best_rmse = rmse
+        best_model = model
+
+
+# PREDICTION
+# %%
+model = tf.keras.models.load_model('./output/modello.h5')
+audio_test = training.librosa.load(
+    './output/train/808_38.wav', sr=training.TARGET_SR)
+prediction = model.predict(np.expand_dims(audio_test[0], axis=0))
+# %%
--- python/src/neural_network/training.py
+++ python/src/neural_network/training.py
@@ -1,4 +1,5 @@
 # %%
+import wave
 import librosa
 import os
 import numpy as np
@@ -19,8 +20,8 @@ OUTPUT_DIR_TEST = os.path.join(OUTPUT_DIR, 'test')
 
 
 def read_audio_from_filename(filename):
-    audio = [[0 for j in range(22050)] for i in range(61)]
-    for i in range(61):
+    audio = [[0 for j in range(22050)] for i in range(60)]
+    for i in range(60):
         line = librosa.load(filename,
                             offset=15.0+i/2, duration=0.5, sr=TARGET_SR)
         audio[i] = list(line)
@@ -32,63 +33,46 @@ def read_audio_from_filename(filename):
 
 # %%
 def convert_data():
-    path,_,_ = extract_input_target()
+    path, _, _ = extract_input_target()
     for x_i in path:
         print(x_i)
-        audio_buf = read_audio_from_filename(os.path.join(DATA_AUDIO_DIR, (x_i+'.mp3')))
-        for k, (audio_sample)in enumerate(audio_buf):
-            # Zero padding if the sample is short
+        audio_buf = read_audio_from_filename(
+            os.path.join(DATA_AUDIO_DIR, (x_i+'.mp3')))
+        for k, (audio_sample) in enumerate(audio_buf):
+            # Zero padding if the sample is short)
             if len(audio_sample[0]) < AUDIO_LENGTH:
                 # print(audio_sample[0])
                 audio_sample = (np.concatenate((audio_sample[0], np.zeros(
                     shape=(AUDIO_LENGTH - len(audio_sample[0]))))), TARGET_SR)
 
-
             output_folder = OUTPUT_DIR_TRAIN
             output_filename = os.path.join(
                 output_folder, str(x_i) + str('_') + str(k)+'.wav')
-            sf.write(output_filename,audio_sample[0] ,TARGET_SR, subtype='PCM_24')
-
+            sf.write(output_filename,
+                     audio_sample[0], TARGET_SR, subtype='PCM_16')
 
 
 # %%
 def extract_input_target():
     path_arousal = '../../../Music_mp3/annotations/arousal_cont_average.csv'
     path_valence = '../../../Music_mp3/annotations/valence_cont_average.csv'
-    path_info = '../../../Music_mp3/annotations/songs_info.csv'
 
     data_arousal = pd.read_csv(path_arousal)
     data_valence = pd.read_csv(path_valence)
-    data_info = pd.read_csv(path_info)
 
     path = data_arousal['song_id'].apply(lambda x: str(x))
 
     arousal = data_arousal['sample_'+str(150)+'00ms']
 
-    arousal = np.zeros(((744, 61)))
-    valance = np.zeros((744, 61))
+    arousal = np.zeros(((744, 60)))
+    valance = np.zeros((744, 60))
 
     for j in range(744):
-        for i in range(61):
+        for i in range(60):
             arousal[j][i] = data_arousal['sample_'+str(150+i*5)+'00ms'][j]
             valance[j][i] = data_valence['sample_'+str(150+i*5)+'00ms'][j]
 
     return path, arousal, valance
 
-
-# %%
-def get_data(file_list):
-    def load_into(_filename, _audio, _valence, _arousal):
-        with open(_filename, 'rb') as f:
-            audio_element = pickle.load(f)
-            _audio.append(audio_element['audio'])
-            _valence.append(audio_element['valence'])
-            _arousal.append(audio_element['arousal'])
-
-    x, valence, arousal = [], [], []
-    for filename in file_list:
-        load_into(filename, x, valence, arousal)
-    print('GET_DATA')
-    return np.array(x), np.array(valence), np.array(arousal)
 
 # %%
