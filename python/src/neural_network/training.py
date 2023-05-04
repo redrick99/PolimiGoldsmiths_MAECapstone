745c6df993912a9b9576f9e8ca38059bbfe53c5f
Francesco <francesco.piferi@mail.polimi.it>
2023-05-04 12:21:21 +0200

    Augmented dataset: added +/-1 semitone each song
    Co-authored-by: Ferdinando222 <Ferdinando222@users.noreply.github.com>

--- python/src/neural_network/training.py
+++ python/src/neural_network/training.py
@@ -2,6 +2,7 @@
 import wave
 import librosa
 import os
+import random
 import numpy as np
 import pickle
 import glob
@@ -18,14 +19,14 @@ OUTPUT_DIR_TEST = os.path.join(OUTPUT_DIR, 'test')
 
 # %%
 
-
-def read_audio_from_filename(filename):
+# shift by default is set to 0, by changing it the pitch shift is applied
+def read_audio_from_filename(filename, shift = 0):
     audio = [[0 for j in range(22050)] for i in range(60)]
     for i in range(60):
         line = librosa.load(filename,
                             offset=15.0+i/2, duration=0.5, sr=TARGET_SR)
-        audio[i] = list(line)
-        audio[i] = (librosa.util.normalize(audio[i][0]), TARGET_SR)
+        line = librosa.effects.pitch_shift(line[0], sr = TARGET_SR, n_steps = shift)
+        audio[i] = librosa.util.normalize(line)
     # audio is a matrix composed by 22050 columns and 60 rows. Each row contains the samples of 0.5 seconds
     # of the track from 15.0s to 45.0s
     return audio
@@ -38,18 +39,29 @@ def convert_data():
         print(x_i)
         audio_buf = read_audio_from_filename(
             os.path.join(DATA_AUDIO_DIR, (x_i+'.mp3')))
-        for k, (audio_sample) in enumerate(audio_buf):
+        audio_buf_shift = read_audio_from_filename(
+            os.path.join(DATA_AUDIO_DIR, (x_i+'.mp3')), shift=random.choice([-1,1]))
+        for k, (audio_sample, audio_sample_shift) in enumerate(zip(audio_buf, audio_buf_shift)):
             # Zero padding if the sample is short)
-            if len(audio_sample[0]) < AUDIO_LENGTH:
+            if len(audio_sample) < AUDIO_LENGTH:
                 # print(audio_sample[0])
-                audio_sample = (np.concatenate((audio_sample[0], np.zeros(
-                    shape=(AUDIO_LENGTH - len(audio_sample[0]))))), TARGET_SR)
+                audio_sample = np.concatenate((audio_sample, np.zeros(
+                    shape=(AUDIO_LENGTH - len(audio_sample)))))
+            if len(audio_sample_shift) < AUDIO_LENGTH:
+                audio_sample_shift = np.concatenate((audio_sample_shift, np.zeros(
+                    shape=(AUDIO_LENGTH - len(audio_sample_shift)))))
 
             output_folder = OUTPUT_DIR_TRAIN
             output_filename = os.path.join(
                 output_folder, str(x_i) + str('_') + str(k)+'.wav')
             sf.write(output_filename,
-                     audio_sample[0], TARGET_SR, subtype='PCM_16')
+                     audio_sample, TARGET_SR, subtype='PCM_16')
+            
+            output_filename = os.path.join(
+                output_folder, str(int(x_i)+1000) + str('_') + str(k)+'.wav')
+            sf.write(output_filename,
+                     audio_sample_shift, TARGET_SR, subtype='PCM_16')
+            
 
 
 # %%
@@ -75,4 +87,6 @@ def extract_input_target():
     return path, arousal, valance
 
 
+# %%
+convert_data()
 # %%
