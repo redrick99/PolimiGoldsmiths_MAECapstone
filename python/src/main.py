66c376e7388b5103c8aec7695c8ec40c3bcc8d51
redrick99 <10628467@polimi.it>
2023-05-25 10:29:06 +0200

    Added docs for audio_producer.py

--- python/src/main.py
+++ python/src/main.py
@@ -11,8 +11,14 @@ from modules.custom_exceptions import *
 
 
 def stop_execution(lf_queue: Queue, hf_queue: Queue, streams: list):
+    """ Stops all concurrent worker processes
+
+    :param lf_queue: Common queue for all Low Level Features workers
+    :param hf_queue: Common queue for all High Level Features workers
+    :param streams: All currently open streams of audio
+    """
     for _ in range(dp.CPU_PARAMETERS['numLfCores']):
-        lf_queue.put(None)
+        lf_queue.put(None)  # When the worker reads `None` from the queue it breaks its loop
     for _ in range(dp.CPU_PARAMETERS['numHfCores']):
         hf_queue.put(None)
     for s in streams:
@@ -22,6 +28,14 @@ def stop_execution(lf_queue: Queue, hf_queue: Queue, streams: list):
 
 
 def audio_producer(audio_producer_object: AudioProducer, control_event, lf_queue: Queue, hf_queue: Queue, parameters: dict):
+    """ Produces audio for the worker processes.
+
+    :param audio_producer_object: Object of the super-class AudioProducer used to produce audio
+    :param control_event: Event used to check if the program's execution has to be stopped
+    :param lf_queue: Common queue for LLF workers used to send the chunks of audio to process
+    :param hf_queue: Common queue for HLF workers used to send the chunks of audio to process
+    :param parameters: Dictionary containing audio processing parameters used to produce audio
+    """
     sh = SetupHandler.get_instance()
     sh.set_audio_parameters(parameters)
     in_stream, out_stream = sh.get_audio_streams()
@@ -64,6 +78,13 @@ def audio_producer(audio_producer_object: AudioProducer, control_event, lf_queue
 
 
 def lf_audio_consumer(lf_queue: Queue, settings_queue: Queue, parameters: dict):
+    """ Processes LLF from audio chunks given from the audio_producer process
+
+    :param lf_queue: Queue where audio chunks to process are sent by the audio_producer process
+    :param settings_queue: Queue where audio settings (coming from osc messages) are sent - used to change settings
+    of the LLF handlers
+    :param parameters: Dictionary containing audio processing parameters used to produce audio
+    """
     lf_audio_input_handlers = []
     channels = parameters['channels']
     instruments = parameters['instruments']
@@ -95,6 +116,11 @@ def lf_audio_consumer(lf_queue: Queue, settings_queue: Queue, parameters: dict):
 
 
 def hf_audio_consumer(hf_queue: Queue, parameters: dict):
+    """ Processes HLF from audio chunks given from the audio_producer process
+
+    :param hf_queue: Queue where audio chunks to process are sent by the audio_producer process
+    :param parameters: Dictionary containing audio processing parameters used to produce audio
+    """
     hf_audio_input_handler = HFAudioInputHandler(parameters, 0, Instruments.DEFAULT)
 
     print_success("Started HF consumer process")
@@ -111,19 +137,18 @@ def hf_audio_consumer(hf_queue: Queue, parameters: dict):
 
 
 if __name__ == "__main__":
-    sh = SetupHandler.get_instance()
+    sh = SetupHandler.get_instance()  # General setup of the application is handled by the SetupHandler singleton
     sh.set_main_path(os.path.dirname(__file__))
     parameters = sh.setup()
     audio_producer_object = sh.get_audio_producer()
 
-    control_event = Event()
-
+    control_event = Event()  # Initializes multiprocessing objects
     cpu_parameters = dp.CPU_PARAMETERS
     lf_queue = Queue()
     hf_queue = Queue()
     settings_queue = Queue()
 
-    processes = []
+    processes = []  # Initializes processes based on the number of wanted parallel workers
     for _ in range(cpu_parameters['numLfCores']):
         processes.append(Process(target=lf_audio_consumer, args=(
             lf_queue,
@@ -144,8 +169,8 @@ if __name__ == "__main__":
         parameters,
     )))
 
-    for p in processes:
+    for p in processes:  # Starts processes
         p.start()
 
-    for p in processes:
+    for p in processes:  # Waits for processes to end
         p.join()
--- python/src/modules/audio_producer.py
+++ python/src/modules/audio_producer.py
@@ -6,8 +6,14 @@ from modules.custom_exceptions import *
 
 
 class AudioProducer(ABC):
+    """ Abstract class representing an input audio handler
+    """
 
     def __init__(self, parameters: dict):
+        """ Constructor for the AudioProducer class
+
+        :param parameters: A dictionary containing audio parameters
+        """
         self._sample_rate = parameters['sampleRate']
         self._chunk_size = parameters['chunkSize']
         self._sample_format = parameters['sampleFormat']
@@ -16,43 +22,77 @@ class AudioProducer(ABC):
 
     @abstractmethod
     def get_next_chunk(self, in_stream: pyaudio.Stream, out_stream: pyaudio.Stream) -> list:
+        """ Abstract function to get the next chunk of audio from input
+
+        :param in_stream: Input stream to use for live audio applications
+        :param out_stream: Output stream used to playback audio in recorded applications
+
+        :returns: The next chunk of audio divided by input channel
+        """
         pass
 
 
 class LiveAudioProducer(AudioProducer):
+    """  Handles input audio taken live from the input sound card
+    """
 
     def __init__(self, parameters: dict):
+        """ Constructor for the LiveAudioProducer class
+
+        :param parameters: A dictionary containing audio parameters
+        """
         super().__init__(parameters)
         
     def get_next_chunk(self, in_stream: pyaudio.Stream, out_stream: pyaudio.Stream) -> list:
+        """ Gets the next chunk of audio from input
+
+        :param in_stream: Input stream to use for live audio applications
+        :param out_stream: Output stream used to playback audio in recorded applications
+
+        :returns: The next chunk of audio divided by input channel
+        """
         data_per_channel = []
 
         if in_stream is None: 
             raise AudioProducingException("Input stream was None")
         
-        chunk_bytes = in_stream.read(self._chunk_size, False)
-        chunk_array = np.frombuffer(chunk_bytes, dtype=self._np_format)
+        chunk_bytes = in_stream.read(self._chunk_size, False)  # Reads from input stream
+        chunk_array = np.frombuffer(chunk_bytes, dtype=self._np_format)  # Converts to numpy array
 
-        for i in range(self._channels):
+        for i in range(self._channels):  # Divides master audio chunk into channels
             data_per_channel.append(chunk_array[i::self._channels])
 
         return data_per_channel
 
 
 class RecordedAudioProducer(AudioProducer):
+    """  Handles recorded audio taken from wav audio files
+    """
 
     def __init__(self, parameters: dict):
+        """ Constructor for the RecordedAudioProducer class
+
+        :param parameters: A dictionary containing audio parameters
+        """
         super().__init__(parameters)
 
         self._audio_input_tracks = parameters['tracks']
         self._audio_playback = parameters['audioPlayback']
 
     def get_next_chunk(self, in_stream: pyaudio.Stream, out_stream: pyaudio.Stream) -> list:
+        """ Gets the next chunk of audio from input
+
+        :param in_stream: Input stream to use for live audio applications
+        :param out_stream: Output stream used to playback audio in recorded applications
+
+        :returns: The next chunk of audio divided by input channel
+        """
         data_per_channel = []
         cs = self._chunk_size
         
         for i in range(len(self._audio_input_tracks)):
-            if len(self._audio_input_tracks[i]) == 0: raise FinishedSongException("Tried to access finished song")
+            if len(self._audio_input_tracks[i]) == 0:
+                raise FinishedSongException("Tried to access finished song")
             if len(self._audio_input_tracks[i]) < cs:
                 data_per_channel.append(self._audio_input_tracks[i].copy())
                 self._audio_input_tracks[i] = []
@@ -60,14 +100,19 @@ class RecordedAudioProducer(AudioProducer):
             data_per_channel.append(self._audio_input_tracks[i][0:cs])
             self._audio_input_tracks[i] = self._audio_input_tracks[i][cs:len(self._audio_input_tracks[i])]
 
-        if out_stream:
+        if out_stream:  # Plays audio if the user choose to do so during startup
             self.__play_audio_chunk(data_per_channel, out_stream)
-        else:
+        else:  # Waits for chunk_size/sample_rate otherwise (to mimic live audio behaviour)
             time.sleep(np.abs(cs/self._sample_rate))
 
         return data_per_channel
 
     def __play_audio_chunk(self, data_per_channel: list, out_stream: pyaudio.Stream):
+        """ Plays the audio chunk on an output stream
+
+        :param data_per_channel: Audio chunk divided by channel
+        :param out_stream: Output stream used to play the audio chunk
+        """
         np_data = np.array(data_per_channel, dtype=np.float32, copy=True)
         np_data = np_data.sum(axis=0) / float(len(data_per_channel))
 
