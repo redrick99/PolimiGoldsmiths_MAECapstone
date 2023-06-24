ce972e0305658c92848911a7c06b514ad7cec9dc
Ferdinando222 <71137846+Ferdinando222@users.noreply.github.com>
2023-06-24 10:55:47 +0200

    Added documentation and cleaned the code

--- python/src/neural_network/functions_train.py
+++ python/src/neural_network/functions_train.py
@@ -2,58 +2,85 @@ import pandas as pd
 import glob
 import os
 import tensorflow as tf
-from keras.layers import Input, Conv1D, MaxPooling1D, BatchNormalization, Concatenate
+import numpy as np
+from keras.layers import Input, Conv1D, MaxPooling1D, BatchNormalization, Concatenate,AveragePooling1D
 from keras.layers import Bidirectional, LSTM, Dropout, Dense, ZeroPadding1D
+from tensorflow.keras.regularizers import l2
 
 
 # DEFINE VARIABLES
-OUTPUT_DIR = '/kaggle/input/wav-16bit/train/'
+INPUT_DIR = '/kaggle/input/wav-16bit/train/'
 AUTOTUNE = tf.data.experimental.AUTOTUNE
-# Define the strategy to use
-strategy = tf.distribute.MirroredStrategy(devices=["/gpu:0", "/gpu:1"])
-# DEFINE FUNCTIONS
-
 
 def extract_input_target():
-    path_arousal = '/kaggle/input/csv-mae/arousal_cont_average.csv'
-    path_valence = '/kaggle/input/csv-mae/valence_cont_average.csv'
+    """Linking the audio path with the associated valence-arousal
+
+    **Returns:**
+    The path of the audio and its corresponding labels
+
+    """
+    path_arousal = '/kaggle/input/deam-mediaeval-dataset-emotional-analysis-in-music/DEAM_Annotations/annotations/annotations averaged per song/dynamic (per second annotations)/arousal.csv'
+    path_valence = '/kaggle/input/deam-mediaeval-dataset-emotional-analysis-in-music/DEAM_Annotations/annotations/annotations averaged per song/dynamic (per second annotations)/valence.csv'
 
     data_arousal = pd.read_csv(path_arousal)
     data_valence = pd.read_csv(path_valence)
-    arousal = data_arousal.drop(columns=['song_id', 'sample_45000ms'])
-    valence = data_valence.drop(columns=['song_id', 'sample_45000ms'])
-    path = glob.glob(os.path.join(OUTPUT_DIR, '*.wav'))
-    final_path = sorted(path, key=lambda x: (int(x.split(
-        '/')[-1].split('_')[0]), int(x.split('/')[-1].split('_')[-1].split('.')[0])))
+    arousal = data_arousal.drop(columns =['song_id'])
+    valence = data_valence.drop(columns = ['song_id'])
+    arousal = arousal.iloc[:, :60]
+    valence = valence.iloc[:,:60]
+    path = glob.glob(os.path.join(INPUT_DIR, '*.wav'))
+    final_path= sorted(path, key=lambda x: (int(x.split('/')[-1].split('_')[0]),int(x.split('/')[-1].split('_')[-1].split('.')[0])))
     arousal = arousal.values.flatten()
+    arousal = np.concatenate((arousal,arousal))
     valence = valence.values.flatten()
-
-    labels = list(zip(arousal, valence))
-    return final_path, labels
-
-
-def read_audio_files(filename, labels):
-    audio_path = tf.io.read_file(filename)
-    audio, _ = tf.audio.decode_wav(
-        audio_path, desired_channels=1, desired_samples=44100)
-    return audio, labels
-
+    valence = np.concatenate((valence,valence))
+    
+    labels = list(zip(arousal,valence))
+    return final_path,labels
 
 def get_dataset(path, labels):
+    """Creates a dataset by zipping audio file paths and their corresponding labels.
+
+    **Args:**
+        path (List[str]): List of file paths of audio files.
+        labels (List[str]): List of labels associated with the audio files.
+
+    **Returns:**
+        dataset (tf.data.Dataset): A TensorFlow dataset containing zipped file paths and labels.
+    """
     file_path_ds = tf.data.Dataset.from_tensor_slices(path)
     label_ds = tf.data.Dataset.from_tensor_slices(labels)
     return tf.data.Dataset.zip((file_path_ds, label_ds))
 
-
 def load_audio(file_path, label):
+    """Reads audio files and their corresponding labels.
+
+    **Args:**
+        file_path (str): File path of the audio file.
+        labels (str): Label associated with the audio file.
+
+    **Returns:**
+        audio (Tensor): Audio data as a TensorFlow Tensor.
+        labels (str): Corresponding labels for the audio file.
+    """
     audio = tf.io.read_file(file_path)
-    audio, _ = tf.audio.decode_wav(audio,
-                                   desired_channels=-1,
-                                   desired_samples=-1)
+    audio, _ = tf.audio.decode_wav(audio)
     return audio, label
 
 
 def prepare_for_training(ds, shuffle_buffer_size=1024, batch_size=64):
+
+    """Prepares a dataset for training by applying transformations.
+
+    **Args:**
+        ds (tf.data.Dataset): The input dataset.
+        shuffle_buffer_size (int): The buffer size for shuffling the dataset.
+        batch_size (int): The batch size for creating batches of data.
+
+    **Returns:**
+        ds (tf.data.Dataset): The prepared dataset.
+    """
+
     # Randomly shuffle (file_path, label) dataset
     ds = ds.shuffle(buffer_size=shuffle_buffer_size)
     # Load and decode audio from file paths
@@ -66,45 +93,79 @@ def prepare_for_training(ds, shuffle_buffer_size=1024, batch_size=64):
 
     return ds
 
+def R_squared(y, y_pred):
+    """Calculates the coefficient of determination (R-squared).
+
+    **Args:**
+        y (tf.Tensor): The true values.
+        y_pred (tf.Tensor): The predicted values.
+
+    **Returns:**
+        r2 (tf.Tensor): The R-squared value.
+    """
+  
+    residual = tf.reduce_sum(tf.square(tf.subtract(y, y_pred)))
+    total = tf.reduce_sum(tf.square(tf.subtract(y, tf.reduce_mean(y))))
+    r2 = tf.subtract(1.0, residual/total)
+    return r2
 
 def create_model():
-    with strategy.scope():
-        # Define input shape
-        input_shape = (22050, 1)
+    """Creates Model.
 
-        # Define input layer
-        input_layer = Input(shape=input_shape)
+    **Returns:**
+        model (tf.keras.Model): The created model.
+    """
 
-        # Define fine-view CNN
-        fine_view = Conv1D(filters=8, kernel_size=32,
-                           strides=8, activation='relu')(input_layer)
-        fine_view = BatchNormalization()(fine_view)
-        fine_view = MaxPooling1D(pool_size=8)(fine_view)
+    # Define input shape
+    input_shape = (22050, 1)
 
-        # Define coarse-view CNN
-        coarse_view = Conv1D(filters=8, kernel_size=128,
-                             strides=32, activation='relu')(input_layer)
-        coarse_view = BatchNormalization()(coarse_view)
-        coarse_view = MaxPooling1D(pool_size=2)(coarse_view)
+    # Define input layer
+    input_layer = Input(shape=input_shape)
 
-        # Pad the coarse-view CNN to match the number of time steps in the fine-view CNN
-        coarse_view = ZeroPadding1D(padding=((0, 1)))(coarse_view)
 
-        # Merge the two CNNs
-        merged = Concatenate(axis=-1)([fine_view, coarse_view])
+    # Define fine-view CNN
+    fine_view = Conv1D(filters=20, kernel_size=256, strides=32, activation='relu', kernel_regularizer=l2(0.0001), kernel_initializer='normal')(input_layer)
+    fine_view = BatchNormalization()(fine_view)
+    fine_view = AveragePooling1D(pool_size=8)(fine_view)
 
-        # Define Bidirectional LSTM layers
-        lstm = Dropout(rate=0.5)(merged)
-        lstm = Bidirectional(LSTM(units=32, return_sequences=True))(lstm)
-        lstm = Bidirectional(LSTM(units=32))(lstm)
-        lstm = Dropout(rate=0.5)(lstm)
+    # Define fine-view CNN 1
+    fine_view1 = Conv1D(filters=20, kernel_size=256, strides=32, activation='relu', kernel_regularizer=l2(0.0001), kernel_initializer='normal')(input_layer)
+    fine_view1 = BatchNormalization()(fine_view1)
+    fine_view1 = AveragePooling1D(pool_size=8)(fine_view1)
 
-        # Define output layer
-        output_layer = Dense(units=2, activation='tanh')(lstm)
+    # Define fine-view CNN 2
+    fine_view2 = Conv1D(filters=20,kernel_size=512, strides=64, activation='relu', kernel_regularizer=l2(0.0001), kernel_initializer='normal')(input_layer)
+    fine_view2 = BatchNormalization()(fine_view2)
+    fine_view2 = AveragePooling1D(pool_size=4)(fine_view2)
+    
+    # Define fine-view CNN 2
+    fine_view3 = Conv1D(filters=20,kernel_size=512, strides=64, activation='relu', kernel_regularizer=l2(0.0001), kernel_initializer='normal')(input_layer)
+    fine_view3 = BatchNormalization()(fine_view3)
+    fine_view3 = AveragePooling1D(pool_size=4)(fine_view3)
+    
+    #fine_view1 = ZeroPadding1D(padding=((0, 1)))(fine_view1)
+    fine_view2 = ZeroPadding1D(padding=((0, 1)))(fine_view2)
+    fine_view3 = ZeroPadding1D(padding=((0, 1)))(fine_view3)
 
-        # Define the final model
-        model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
-        model.compile(optimizer='adam', loss='mse', metrics=[
-                      'mse', tf.keras.metrics.RootMeanSquaredError()], run_eagerly=True)
-        model.summary()
+    # Merge the three CNNs
+    merged = Concatenate(axis=-1)([fine_view, fine_view1, fine_view2,fine_view3])
+
+    # Define Bidirectional LSTM layers
+    lstm = Dropout(rate=0.5)(merged)
+    lstm = Bidirectional(LSTM(units=32, return_sequences=True))(lstm)
+    lstm = Bidirectional(LSTM(units=32))(lstm)
+    lstm = Dropout(rate=0.5)(lstm)
+    lstm = tf.expand_dims(lstm, axis=1)
+    lstm = Bidirectional(LSTM(units=32, return_sequences=True))(lstm)
+    lstm = Bidirectional(LSTM(units=32))(lstm)
+    lstm = Dropout(rate=0.5)(lstm)
+
+    # Define output layer
+    output_layer = Dense(units=2, activation='tanh')(lstm)
+
+    # Define the final model
+    model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
+    optimizer = tf.keras.optimizers.Adam(lr=0.0001)
+    model.compile(optimizer=optimizer, loss='mse', metrics=['mse', tf.keras.metrics.RootMeanSquaredError(), 'accuracy',R_squared], run_eagerly=True)
+    model.summary()
     return model
--- python/src/neural_network/training.py
+++ python/src/neural_network/training.py
@@ -1,5 +1,3 @@
-# %%
-
 # This script is used to convert the audio in frames of 0.5 seconds
 # with a total length of 45 seconds
 
@@ -8,8 +6,8 @@ import os
 import numpy as np
 import pandas as pd
 import soundfile as sf
+import random
 
-# CHOOSE THE DATA AUDIO DIRECTION
 DATA_AUDIO_DIR = '../../../Music_mp3/DEAM_audio/MEMD_audio'
 TARGET_SR = 44100
 AUDIO_LENGTH = 22050
@@ -17,62 +15,93 @@ OUTPUT_DIR = './output'
 OUTPUT_DIR_TRAIN = os.path.join(OUTPUT_DIR, 'train')
 OUTPUT_DIR_TEST = os.path.join(OUTPUT_DIR, 'test')
 
-# %%
 
-# shift by default is set to 0, by changing it the pitch shift is applied
 def read_audio_from_filename(filename, shift = 0):
+    """Read audio from the specified path and .
+
+    **Args:**
+
+    `filename`: Path of the audio to read.
+
+    `shift`: Argument used only if we want perform data augumentation. Shift the song with the selected number of semitones.
+
+    **Returns:**
+    Audio is a matrix composed by 22050 columns and 60 rows. Each row contains the samples of 0.5 seconds
+    of the track from 15.0s to 45.0s.
+
+    """
     audio = []
     for i in range(60):
         line = librosa.load(filename,
                             offset=15.0+i/2, duration=0.5, sr=TARGET_SR)
         line = librosa.effects.pitch_shift(line[0], sr = TARGET_SR, n_steps = shift)
         audio.append(librosa.util.normalize(line))
-    # audio is a matrix composed by 22050 columns and 60 rows. Each row contains the samples of 0.5 seconds
-    # of the track from 15.0s to 45.0s
     return audio
 
+def convert_data(data_augumentation = 0):
+    """Reads audio from the specified path and converts it to WAV format.
 
-# %%
-def convert_data():
+    **Args:**
+        data_augmentation (int): Flag indicating whether to perform data augmentation. Default is 0 (no data augmentation).
+
+    **Returns:**
+        None
+    """
+
     path = extract_input_target()
     for x_i in path:
         print(x_i)
         audio_buf = read_audio_from_filename(
             os.path.join(DATA_AUDIO_DIR, (x_i+'.mp3')))
-        # PITCH SHIFT FOR DATA AUGUMENTATION
-        # audio_buf_shift = read_audio_from_filename(
-        # os.path.join(DATA_AUDIO_DIR, (x_i+'.mp3')), shift=random.choice([-1,1]))
-        for k, (audio_sample) in enumerate(audio_buf):
         
-        # PITCH SHIFT FOR DATA AUGUMENTATION
-        # for k, (audio_sample, audio_sample_shift) in enumerate(zip(audio_buf, audio_buf_shift)):
+        #With Data augumentation
+        if(data_augumentation == 1):
+            audio_buf_shift = read_audio_from_filename(
+            os.path.join(DATA_AUDIO_DIR, (x_i+'.mp3')), shift=random.choice([-1,1]))
+            for k, (audio_sample, audio_sample_shift) in enumerate(zip(audio_buf, audio_buf_shift)):
             
-            # Zero padding if the sample is short)
+                # Zero padding if the sample is short)
+
+                if len(audio_sample) < AUDIO_LENGTH:
+                    audio_sample = np.concatenate((audio_sample, np.zeros(
+                    shape=(AUDIO_LENGTH - len(audio_sample)))))
+            
+                if len(audio_sample_shift) < AUDIO_LENGTH:
+                    audio_sample_shift = np.concatenate((audio_sample_shift, np.zeros(
+                    shape=(AUDIO_LENGTH - len(audio_sample_shift)))))
+
+                output_folder = OUTPUT_DIR_TRAIN
+                output_filename = os.path.join(
+                output_folder, str(x_i) + str('_') + str(k)+'.wav')
+                sf.write(output_filename,
+                     audio_sample, TARGET_SR, subtype='PCM_16')
+
+                output_filename = os.path.join(
+                output_folder, str(int(x_i)+1000) + str('_') + str(k)+'.wav')
+                sf.write(output_filename,
+                    audio_sample_shift, TARGET_SR, subtype='PCM_16')
+
+        for k, (audio_sample) in enumerate(audio_buf):
+            
+            # Zero padding if the sample is short
 
             if len(audio_sample) < AUDIO_LENGTH:
                 audio_sample = np.concatenate((audio_sample, np.zeros(
                     shape=(AUDIO_LENGTH - len(audio_sample)))))
             
-            # PITCH SHIFT FOR DATA AUGUMENTATION
-            # if len(audio_sample_shift) < AUDIO_LENGTH:
-            # audio_sample_shift = np.concatenate((audio_sample_shift, np.zeros(
-            # shape=(AUDIO_LENGTH - len(audio_sample_shift)))))
 
             output_folder = OUTPUT_DIR_TRAIN
             output_filename = os.path.join(
                 output_folder, str(x_i) + str('_') + str(k)+'.wav')
             sf.write(output_filename,
                      audio_sample, TARGET_SR, subtype='PCM_16')
-            
-            # PITCH SHIFT FOR DATA AUGUMENTATION
-
-            # output_filename = os.path.join(
-            # output_folder, str(int(x_i)+1000) + str('_') + str(k)+'.wav')
-            # sf.write(output_filename,
-            #          audio_sample_shift, TARGET_SR, subtype='PCM_16')
-            
-# %%
+        
 def extract_input_target():
+    """Extract the path of each songs .
+
+    **Returns:**
+    The path of each song
+    """
     path_arousal = r'../../../Music_mp3/DEAM_Annotations/annotations/annotations averaged per song/dynamic (per second annotations)/arousal.csv'
 
     data_arousal = pd.read_csv(path_arousal)
@@ -80,8 +109,3 @@ def extract_input_target():
     path = data_arousal['song_id'].apply(lambda x: str(x))
 
     return path
-
-
-# %%
-convert_data()
-# %%
